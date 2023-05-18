d97712f5dfca167076335464a38a271b80318a32
PESokolova <PESokolova@inno.tech>
2023-05-18 19:04:08 +0300

    add refactor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,12 @@ public class Main {
         Frame frame1 = new Frame();
         frame1.shape = "round";
         frame1.color = "white";
+        bike1.frame = frame1;
 
         Wheel wheel1 = new Wheel();
         wheel1.diameter = 65;
         wheel1.number = 2;
+        bike1.wheel = wheel1;
 
     }
 }
